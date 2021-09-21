ce4bbcb49576313f09bc262bb9ce57ef8a0e309a
prog-apprentice401 <61466761+prog-apprentice401@users.noreply.github.com>
2021-09-21 20:14:29 +0530

    Add files via upload

---
+++ reciever/transmitter/transmitter.ino
@@ -0,0 +1,40 @@
+#define POT_PIN A0
+#define OUTPUT_PIN 2
+#define SIGNAL_PIN 3
+
+uint8_t potValue;
+void sendThroughLaser (const byte, uint8_t);
+
+void setup ()
+{
+	Serial.begin (9600);
+	pinMode (POT_PIN, INPUT);
+	pinMode (OUTPUT_PIN, OUTPUT);
+	pinMode (SIGNAL_PIN, INPUT);
+	digitalWrite (OUTPUT_PIN, LOW);
+}
+
+void loop ()
+{
+	while (!digitalRead (SIGNAL_PIN));
+
+	potValue = map (analogRead (POT_PIN), 0, 1023, 0, 255);
+	sendThroughLaser (OUTPUT_PIN, potValue);
+}
+
+void sendThroughLaser (const byte outputPin, uint8_t dataToSend)
+{
+	bool currentOutputState = LOW;
+	uint8_t temp = dataToSend;
+	for (int i = 0; i < 8; i++){
+		digitalWrite (outputPin, !currentOutputState);
+		delay (50); //signalling new bit
+		currentOutputState = temp & B00000001;	//access LSB of data
+		temp >>= 1;
+		digitalWrite (outputPin, currentOutputState);	
+		delay (100);
+		Serial.println (currentOutputState);
+	}
+	digitalWrite (outputPin, LOW);
+	Serial.println (dataToSend, BIN);
+}
